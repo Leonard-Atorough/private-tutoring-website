e1fc45cf8803188c12425b9281c4eb1bc9ba1b8d
Leonard Atorough <leotorough@gmail.com>
2025-07-31 12:46:58 +0100

    update loader to add class name column to wrapper div

--- src/lib/loader.js
+++ src/lib/loader.js
@@ -4,6 +4,7 @@ export async function loadHTML(url) {
       throw new Error(`Failed to load HTML from ${url}: ${response.statusText}`);
    }
    const wrapper = document.createElement("div");
+   wrapper.classList.add("column")
    const text = await response.text();
    wrapper.innerHTML = text;
    return wrapper;
