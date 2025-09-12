f02461322a636acf2de5957d718ff92575930db1
Leonard Atorough <leotorough@gmail.com>
2025-09-12 17:16:27 +0100

    remove uneeded log from header.js

--- src/layout/header.js
+++ src/layout/header.js
@@ -1,6 +1,5 @@
 export function toggleNavMenu(button) {
    const nav = document.querySelector(".nav-menu");
-   console.log(nav);
    nav.classList.toggle("active");
    button.setAttribute("aria-expanded", nav.classList.contains("active"));
 }
