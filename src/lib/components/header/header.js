57f1e354223bc17257aafc9a026e10660ae4f8cc
Leonard Atorough <leotorough@gmail.com>
2025-12-05 18:41:19 +0000

    feat: enhance navigation menu accessibility; update toggle and resize handlers for improved ARIA attributes

--- src/lib/components/header/header.js
+++ src/lib/components/header/header.js
@@ -1,6 +1,18 @@
 const TOGGLE_BUTTON_ID = "menu-toggle";
 const NAVIGATION_MENU_ID = "navigation-menu";
 
+function isNavMenuHidden() {
+  const nav = document.getElementById(NAVIGATION_MENU_ID);
+  return nav.classList.contains("active") === false;
+}
+
+function setNavMenuAccessibilityAttributes() {
+  const nav = document.getElementById(NAVIGATION_MENU_ID);
+  const button = document.getElementById(TOGGLE_BUTTON_ID);
+  nav.setAttribute("aria-hidden", isNavMenuHidden() ? "true" : "false");
+  button.setAttribute("aria-expanded", isNavMenuHidden() ? "false" : "true");
+}
+
 function toggleNavMenu(forceClose) {
   const nav = document.getElementById(NAVIGATION_MENU_ID);
   const button = document.getElementById(TOGGLE_BUTTON_ID);
@@ -9,12 +21,10 @@ function toggleNavMenu(forceClose) {
 
   if (shouldBeActive) {
     nav.classList.add("active");
-    nav.setAttribute("aria-hidden", "false");
   } else {
     nav.classList.remove("active");
-    nav.setAttribute("aria-hidden", "true");
   }
-  button.setAttribute("aria-expanded", nav.classList.contains("active"));
+  setNavMenuAccessibilityAttributes();
 }
 
 let scrollRafId = null;
@@ -39,9 +49,10 @@ function scrollToSection(sectionId) {
 function attachToggleHandler() {
   const toggleButton = document.getElementById(TOGGLE_BUTTON_ID);
   if (!toggleButton) throw Error("Navigation toggle button not found");
-
-  toggleButton.addEventListener("click", () => toggleNavMenu());
-
+  toggleButton.addEventListener("click", () => {
+    toggleNavMenu();
+    setNavMenuAccessibilityAttributes();
+  });
   window.removeEventListener("scroll", () => toggleNavMenu(true));
   window.addEventListener("scroll", () => toggleNavMenu(true));
 }
@@ -59,7 +70,23 @@ function attachScrollHandler() {
   });
 }
 
+function attachResizeHandler() {
+  window.addEventListener("resize", () => {
+    const nav = document.getElementById(NAVIGATION_MENU_ID);
+    if (window.innerWidth < 768) {
+      toggleNavMenu(true); // force close
+      setNavMenuAccessibilityAttributes();
+    } else {
+      // remove accessibility attributes when not in mobile view
+      nav.removeAttribute("aria-hidden");
+      const button = document.getElementById(TOGGLE_BUTTON_ID);
+      button.removeAttribute("aria-expanded");
+    }
+  });
+}
+
 export function initHeader() {
   attachToggleHandler();
   attachScrollHandler();
+  attachResizeHandler();
 }
